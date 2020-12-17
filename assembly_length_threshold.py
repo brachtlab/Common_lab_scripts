7ce4f9da7bfc1c40125a16eece5a6757a3f09fb3
brachtlab <59890682+brachtlab@users.noreply.github.com>
2020-12-17 10:17:30 -0500

    Add files via upload

---
+++ assembly_length_threshold.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+from Bio import SeqIO
+import sys
+from sys import argv
+outHandle=open("%s_over%sbp.fasta"%(argv[1],argv[2]),'w')
+threshold=int(argv[2])
+genome_handle=open("%s"%argv[1],"r")
+for seq_record in SeqIO.parse(genome_handle,"fasta"):
+	seqlength=len(seq_record.seq)
+	if seqlength >=threshold:
+		outHandle.write(">%s\n%s\n"%(seq_record.id,seq_record.seq))
+genome_handle.close()
+outHandle.close()
